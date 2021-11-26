a66ffcab24f222eae752223fa9e427a5762e94ac
Peder <89570194+s364529@users.noreply.github.com>
2021-11-26 02:23:08 +0100

    .

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function navToggle() {
     var navToggle = document.querySelector('.nav-toggle');
-    var navLinks = document.querySelector('.nav-links');
     var navBar = document.querySelector('.navbar');
     
     navToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('navbar-expanded');
         navBar.classList.toggle('navbar-expanded')
-        navToggle.classList.toggle('clicked');
     });
 }
 
